036b27f0e298c0f021f998907ed8627a39bdbe87
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-07-02 21:13:43 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,14 +15,11 @@ function App() {
   const [addWeb, setAddWeb] = useState(false);
 
 
-
-
   function validateUrl(value) {
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
       value
     );
   }
-
 
   
   //add websites to database
@@ -54,7 +51,7 @@ function App() {
     }).catch((err) => void err);
     setInputUrl('');
     setAddWeb(false);
-    // console.log(inputUrl)
+    console.log(inputUrl)
     const result = await res.json();
     if (!result || !result.status) {
       setErrMsg(result.message);
